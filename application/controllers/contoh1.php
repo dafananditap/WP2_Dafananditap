5f2985689f6f226b9db909ba865d9ddbdbea9f84
dafananditap <dafananditap5@gmail.com>
2021-09-23 01:02:21 +0700

    "file-file latihan pertemuan 1"

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class contoh1 extends CI_Controller
+{
+    public function index()
+    {
+    echo "<h1>Perkenalkan</h1>";
+    echo"Nama saya Dafa Nandita Putri
+         saya tinggal di cengkareng
+         hobby saya yaitu maraton drama korea";
+    }
+}
+
